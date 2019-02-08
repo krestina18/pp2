346ba55ac4250780fd6b4652edc13e572969e4f3
unknown <you@example.com>
2019-02-08 23:32:42 +0600

    comment

---
+++ week 1/Task 4/Task 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); //read n
+            for (int i = 0; i < n; i++) //row
+            {
+                for (int j = 0; j <= i; j++) //column numbers in each row increment one after each cycle
+                {
+                    Console.Write("[*]"); //output n time
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
