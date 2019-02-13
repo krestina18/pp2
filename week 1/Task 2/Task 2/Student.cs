3ab709ecb3b9d4c03ea93a36433ec53edc5c176d
unknown <you@example.com>
2019-02-13 19:22:37 +0600

    comment

---
+++ week 1/Task 2/Task 2/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Student
+    {
+        public string name;
+        public string Id;
+        public int YeartOFStudy;
+        public Student(string name, string id) // Constructor which will take two parameters
+        {
+            this.name = name;
+            this.Id = id;
+        }
+        public string GetName() // The Method which returns name of the Student
+        {
+            return name;
+        }
+        public string GetId() // The Method which returns ID of the Student
+        {
+            return Id;
+        }
+        public int GetYearOfStudy()  // The Method which increases Year of the Student
+        {
+            return ++YeartOFStudy;
+        }
+    }
+}
